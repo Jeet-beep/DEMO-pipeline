aaa1f0a3192d35902bd75a916e22be8b4f2ff2d3
Charanjeet Singh <charanjeet23singh2002@gmail.com>
2023-05-18 17:42:00 +0530

    all changes done

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="Census-Income",
+    version="0.0.1",
+    author="charanjeet singh",
+    author_email="charanjeet23singh2002@gmail.com",
+    packages=find_packages(),
+    install_requires=["pandas","numpy","flask"]
+
+
+
+)
